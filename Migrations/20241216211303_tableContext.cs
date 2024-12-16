0ee59cef4307ca24447df4fbb6883a8c03e28318
Delcio_Santos <87926383+charlotte141@users.noreply.github.com>
2024-12-16 22:44:39 +0100

    projecto concluido

---
+++ Migrations/20241216211303_tableContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema_de_Agendamento_de_Tarefas.Migrations
+{
+    /// <inheritdoc />
+    public partial class tableContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
